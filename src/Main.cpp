9ab6f1e2ec2fd840625cd1e37d660b2c540abf1f
lavanderhoney <milapp857@gmail.com>
2024-10-16 20:49:55 +0530

    dict decode trial 1

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,12 +13,17 @@ using namespace std;
 json decode_bencoded_value(const std::string& encoded_value, size_t& position);
 
 json decode_bencoded_string(const string& encoded_string, size_t& idx){
-    size_t length_prefix = encoded_string.find(':');
+    size_t length_prefix = encoded_string.find(':', idx);
     if (length_prefix != string::npos) {
         string string_size_str = encoded_string.substr(idx, length_prefix - idx);
         int64_t string_size_int = atoll(string_size_str.c_str());
-        idx = length_prefix + 1 + string_size_int; // Update idx
-        string str = encoded_string.substr(length_prefix + 1, string_size_int);
+
+        idx = length_prefix + 1; // Move idx to the start of the actual string
+        string str = encoded_string.substr(idx, string_size_int);
+        idx += string_size_int; // Update idx to the end of the string
+
+        // idx = length_prefix + 1 + string_size_int; // Update idx
+        // string str = encoded_string.substr(length_prefix + 1, string_size_int);
         return json(str);
     } else {
         throw runtime_error("Invalid encoded value: " + encoded_string);
@@ -42,26 +47,57 @@ json decode_bencoded_list(const string& encoded_string, size_t& idx){
     idx++;
     json list = json::array();
 
-    while (encoded_string[idx]!='e'){
+    while (idx < encoded_string.size() && encoded_string[idx]!='e'){
         list.push_back(decode_bencoded_value(encoded_string, idx));
     }
-
+    if (idx >= encoded_string.size() || encoded_string[idx] != 'e') {
+        throw runtime_error("Invalid bencoded list: missing 'e'");
+    }
     idx++;
     // cout << list <<endl;
     return list;
 }
 
-json decode_bencoded_dict(const string& encoded_string, size_t& idx){
-    idx++;
-    auto dict = nlohmann::ordered_map<json, json>();
-    while(encoded_string[idx]!='e'){
-        auto key = decode_bencoded_value(encoded_string, idx);
-        auto val = decode_bencoded_value(encoded_string, idx);
-        dict.push_back({key, val});
+json decode_bencoded_dict(const string& encoded_string, size_t& idx) {
+    idx++; // Skip the 'd'
+    json dict = json::object();
+
+    while (encoded_string[idx] != 'e') {
+        // Decode the key (which is always a bencoded string)
+        json key = decode_bencoded_string(encoded_string, idx);
+
+        // Ensure the key is a string, as keys in dictionaries must be strings.
+        if (!key.is_string()) {
+            throw runtime_error("Invalid dictionary key type: " + key.dump());
+        }
+
+        // Decode the corresponding value
+        json value = decode_bencoded_value(encoded_string, idx);
+
+        // Add the key-value pair to the dictionary
+        dict[key.get<string>()] = value;
     }
-    idx++;
-    return json(dict);
+
+    idx++; // Move past 'e' for the next encoded data type
+    return dict;
 }
+// json decode_bencoded_dict(const string& encoded_string, size_t& idx){
+//     idx++;
+//     auto dict = nlohmann::ordered_map<json, json>();
+//     while(encoded_string[idx]!='e' && idx <encoded_string.size()){
+//         auto key = decode_bencoded_value(encoded_string, idx);
+//         auto val = decode_bencoded_value(encoded_string, idx);
+//         if (key.is_null() || val.is_null()) {
+//             throw runtime_error("Invalid key or value encountered while decoding dictionary");
+//         }
+//         // cout << "key, val: " << key << val << endl;
+//         // cout<< "idx" << idx<< endl;
+//         dict.push_back({key, val});
+//     }
+//     idx++;
+//     cout << "the dict is: "<< json(dict) << endl;
+//     return json(dict);
+// }
 
 json decode_bencoded_value(const string& encoded_value, size_t& idx) {
     if (isdigit(encoded_value[idx])) {
@@ -85,28 +121,37 @@ json decode_bencoded_value(const string& encoded_value) {
 
 int main(int argc, char* argv[]) {
     // Flush after every cout / cerr
-    cout << unitbuf;
-    cerr << unitbuf;
+    // cout << unitbuf;
+    // cerr << unitbuf;
 
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " decode <encoded_value>" << endl;
-        return 1;
-    }
+    // if (argc < 2) {
+    //     cerr << "Usage: " << argv[0] << " decode <encoded_value>" << endl;
+    //     return 1;
+    // }
 
-    string command = argv[1];
+    // string command = argv[1];
 
-    if (command == "decode") {
-        if (argc < 3) {
-            cerr << "Usage: " << argv[0] << " decode <encoded_value>" << endl;
-            return 1;
-        }
+    // if (command == "decode") {
+    //     if (argc < 3) {
+    //         cerr << "Usage: " << argv[0] << " decode <encoded_value>" << endl;
+    //         return 1;
+    //     }
     
-        string encoded_value = argv[2];
+    //     string encoded_value = argv[2];
+    //     json decoded_value = decode_bencoded_value(encoded_value);
+    //     cout << decoded_value.dump() << endl;
+    // } else {
+    //     cerr << "unknown command: " << command << endl;
+    //     return 1;
+    // }
+    // return 0;
+     try {
+        string encoded_value = "d3:foo3:bar5:helloi52ee";
         json decoded_value = decode_bencoded_value(encoded_value);
-        cout << decoded_value.dump() << endl;
-    } else {
-        cerr << "unknown command: " << command << endl;
-        return 1;
+        string serial = decoded_value.dump();
+        cout << serial << endl;
+    } catch (const std::exception& e) {
+        cerr << "Error: " << e.what() << endl;
     }
     return 0;
 }
